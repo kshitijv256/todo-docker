3e8b50ef68d558df3f6e8092a954110e59d4ac5e
Kshitij Verma <kshitijv250@gmail.com>
2024-03-06 21:35:33 +0530

    azure deploy 5

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 
 // eslint-disable-next-line no-undef
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 80, () => {
   console.log("Started express server at port 3000");
 });
 // new comment
