25b123042a3ab5339f867e08a531ce611ae6d028
Kshitij Verma <kshitijv250@gmail.com>
2024-03-06 20:14:47 +0530

    azure deploy

--- config/config.js
+++ config/config.js
@@ -16,10 +16,13 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.POSTGRES_USER,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
+    use_env_variable: "DATABASE_URL",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
