ff11c8b963204bb090e14e33b2623a4806a5c1b5
Kshitij Verma <kshitijv250@gmail.com>
2024-03-06 23:04:57 +0530

    new configs 5

--- config/config.js
+++ config/config.js
@@ -12,7 +12,7 @@ module.exports = {
     username: "postgres",
     password: "1234",
     database: "todo_db_test",
-    host: "postgres",
+    host: "127.0.0.1",
     dialect: "postgres",
   },
   production: {
